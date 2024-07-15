42b6bc47f9af994a2458ea05c828639ddc118436
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-07-15 20:50:10 +0100

    Create config.hpp

---
+++ config.hpp
@@ -0,0 +1,27 @@
+#ifndef CONFIG_H
+#define CONFIG_H	1
+#pragma once
+
+/* Comment out this variable if you have a standard board. */
+#define RIPOFF true
+
+#if defined(DSTIKE_DEAUTHER_WATCH) && RIPOFF
+  #define LED_NEOPIXEL_GRB
+  #define LED_PIN             1
+  #define LED_NEOPIXEL_PIN    15
+  #define LED_ON              LOW
+  #define LED_OFF             HIGH
+
+  #define TORCH_PIN           16
+
+  #define SH1106_I2C
+  #define FLIP_DISPLAY         true
+
+  #define BUTTON_UP           13
+  #define BUTTON_DOWN         12
+  #define BUTTON_A            14
+  #define BUTTON_B            0
+  #define BUTTON_C            2
+#endif
+
+#endif
